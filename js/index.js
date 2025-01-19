03d5d7ecbbd3ee544b02f830795b990c7db81210
imdola <dilshaddola2018@gmail.com>
2025-01-19 22:01:10 +0600

    main section js starts

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// Main js code starts 
+
+const noakhaliBalanceElement = document.getElementById("noakhali-balance");
+const feniBalanceElement = document.getElementById("feni-balance");
+const quotaBalanceElement = document.getElementById("quota-balance");
+
+const noakhaliInput = document.getElementById("noakhali-input");
+const feniInput = document.getElementById("feni-input");
+const quotaInput = document.getElementById("quota-input");
+
+const noakhaliButton = document.getElementById("noakhali-button");
+const feniButton = document.getElementById("feni-button");
+const quotaButton = document.getElementById("quota-button");
+
+const modal = document.getElementById("my_modal_1");
+const closeModalButton = document.getElementById("close-modal");
